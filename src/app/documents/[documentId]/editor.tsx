e6167d31e193d245f55d3d221636c76949d653dd
Mohamed Masoud <mo.masoud.7007@gmail.com>
2024-12-15 21:40:40 +0200

    Refactor document page layout by adding a Navbar and adjusting Editor positioning; update overflow behavior in Editor component for improved responsiveness.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -91,7 +91,7 @@ export const Editor = () => {
   });
 
   return (
-    <div className="size-full overflow-x-none bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
       <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
