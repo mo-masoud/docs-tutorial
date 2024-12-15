fa73a0920a9de5d98236dd68679fa6a29c0b9698
Mohamed Masoud <mo.masoud.7007@gmail.com>
2024-12-15 15:02:12 +0200

    Add line height extension and toolbar functionality for adjusting line height options

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,6 +20,7 @@ import ImageResize from "tiptap-extension-resize-image";
 
 import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extensions/font-size";
+import { LineHeightExtension } from "@/extensions/line-height";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -59,6 +60,7 @@ export const Editor = () => {
     extensions: [
       StarterKit,
       FontSizeExtension,
+      LineHeightExtension,
       TextAlign.configure({
         types: ["heading", "paragraph"],
       }),
