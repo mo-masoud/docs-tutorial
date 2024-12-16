b9552a5a84370c17b0b5fb0311e549b9c382a2cf
Mohamed Masoud <mo.masoud.7007@gmail.com>
2024-12-16 22:13:55 +0200

    Update package dependencies to include Convex and related libraries; refactor layout to integrate ConvexClientProvider and enhance Home page with document fetching functionality.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplatesGallary } from "./templates-gallary";
-
+import { api } from "../../../convex/_generated/api";
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (!documents) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +17,9 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplatesGallary />
+        {documents?.map((document) => (
+          <div key={document._id}>{document.title}</div>
+        ))}
       </div>
     </div>
   );
