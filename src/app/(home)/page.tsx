e27c9d7f6c059351172f927d89a365651733ed4d
Mohamed Masoud <mo.masoud.7007@gmail.com>
2024-12-26 07:22:03 +0200

    Enhance document retrieval with search functionality and user authentication
    - Update `get` query in `documents.ts` to include optional search parameter for filtering documents by title.
    - Implement user authentication check to ensure only authorized users can access their documents.
    - Modify Home page to pass search parameter to the paginated document query, improving user experience in document management.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,10 +4,13 @@ import { Navbar } from "./navbar";
 import { TemplatesGallary } from "./templates-gallary";
 import { api } from "../../../convex/_generated/api";
 import { DocumentsTable } from "./documents-table";
+import { useSearchParam } from "@/hooks/use-search-param";
 const Home = () => {
+  const [search] = useSearchParam();
+
   const { results, status, loadMore } = usePaginatedQuery(
     api.documents.get,
-    {},
+    { search },
     { initialNumItems: 5 }
   );
 
